1a31f912ff1c1cf5879070207489a681e1fe9637
devika mukundan <devikamukundan@macbook-pro.mynetworksettings.com>
2025-05-19 22:43:12 -0400

    kik

---
+++ weather-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TypingAnimation from "./TypingAnimation";
+
+function App() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <TypingAnimation />
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-app/src/index.js
@@ -0,0 +1,16 @@
+const styles = {
+  container: {
+    fontFamily: "'Pixelify Sans', sans-serif", // EXACT spelling
+    fontSize: "24px",
+    color: "#222",
+    whiteSpace: "nowrap"
+  }
+};
+
+export default function TypingAnimation() {
+  return (
+    <div style={styles.container}>
+      hello bestie welcome back!
+    </div>
+  );
+}
